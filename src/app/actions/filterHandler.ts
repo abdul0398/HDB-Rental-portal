d9f39c0b299ae0c6c2098514933b414f9f6c0d58
abdul0398 <abdulrkhan570@gmail.com>
2024-04-22 10:32:56 +0530

    Changes the filter logic

--- src/app/actions/filterHandler.ts
+++ src/app/actions/filterHandler.ts
@@ -1,6 +1,6 @@
 // 'use server'
 import data from "@/data/transaction.json";
-import { FilterHandlerParams, filterHandlerReturn } from "@/types/data";
+import { FilterHandlerParams, filterHandlerReturn, transactiontype } from "@/types/data";
 
 export const filterHandler = async  ({
   selectedMonths,
@@ -9,68 +9,149 @@ export const filterHandler = async  ({
   selectedFlatType,
   selectedBlocks,
 }: FilterHandlerParams): Promise<filterHandlerReturn> => {
-  const transactions = data as Array<any>;
-  const filteredTransaction = transactions.filter((transaction, index) => {
+  const transactions = data as Array<transactiontype>;
 
-    if (selectedTown && transaction.town !== selectedTown) {
-      return false;
-    }
-    if (
-      selectedStreetNames.length > 0 &&
-      !selectedStreetNames.includes(transaction.street_name)
-    ) {
-      return false;
-    }
-    if (
-      selectedBlocks.length > 0 &&
-      !selectedBlocks.includes(transaction.block)
-    ) {
-      return false;
-    }
-    if (selectedFlatType && selectedFlatType !== transaction.flat_type) {
-      return false;
-    }
-    if (
-      selectedMonths.length > 0 &&
-      !selectedMonths.includes(transaction.rent_approval_date)
-    ) {
-      return false;
-    }
-    return true;
-  });
+  const filterBlocks: string[] = [];
+  const filterStreets: string[] = [];
+  const filterTowns: string[] = [];
+  const filterFlatTypes:string[] = [];
+  const filterMonths: string[] = [];
 
-  const filterMonths = [
-    ...new Set(
-      filteredTransaction.map((transaction) => transaction.rent_approval_date)
-    ),
-  ];
-  const filterTowns = [
-    ...new Set(filteredTransaction.map((transaction) => transaction.town)),
-  ];
-  const filterStreets = [
-    ...new Set(
-      filteredTransaction.map((transaction) => transaction.street_name)
-    ),
-  ];
-  const filterBlocks = [
-    ...new Set(filteredTransaction.map((transaction) => transaction.block)),
-  ];
-  const filterFlatTypes = [
-    ...new Set(filteredTransaction.map((transaction) => transaction.flat_type)),
-  ];
 
-  filterMonths.sort();
-  filterTowns.sort();
-  filterStreets.sort();
-  filterBlocks.sort();
-  filterFlatTypes.sort();
+ const filteredTransaction : transactiontype[] = [];
+
+  for(const transaction of transactions){
+    let isTownPresent = false;
+    let isStreetPresent = false;
+    let isBlockPresent = false;
+    let isFlatTypePresent = false;
+    let isMonthPresent = false;
+
+
+    const street = transaction.street_name;
+    const town = transaction.town;
+    const block = transaction.block;
+    const flatType = transaction.flat_type;
+    const month = transaction.rent_approval_date;
+    const yearMonth = `20${month.slice(2)}-${month.slice(0, 2)}`
+
+    if (selectedTown.length > 0) {
+      if (selectedTown.includes(town)) {
+        isTownPresent = true;
+      }
+    } else {
+      isTownPresent = true;
+    }
+
+    if (selectedStreetNames.length > 0) {
+      if (selectedStreetNames.includes(street)) {
+        isStreetPresent = true;
+      }
+    } else {
+      isStreetPresent = true;
+    }
+
+    if (selectedBlocks.length > 0) {
+      if (selectedBlocks.includes(block)) {
+        isBlockPresent = true;
+      }
+    } else {
+      isBlockPresent = true;
+    }
+
+    if (selectedFlatType) {
+      if (selectedFlatType === flatType) {
+        isFlatTypePresent = true;
+      }
+    } else {
+      isFlatTypePresent = true;
+    }
+
+    if (selectedMonths.length > 0) {
+      if (selectedMonths.includes(yearMonth)) {
+        isMonthPresent = true;
+      }
+    } else {
+      isMonthPresent = true;
+    }
+
+
+
+
+
+    if (
+      isStreetPresent &&
+      isTownPresent &&
+      isFlatTypePresent &&
+      isMonthPresent
+    ) {
+      filterBlocks.push(block);
+    }
+
+    if (
+      isTownPresent &&
+      isFlatTypePresent &&
+      isMonthPresent && 
+      isBlockPresent
+    ) {
+      filterStreets.push(street);
+    }
+
+    if (
+      isStreetPresent &&
+      isFlatTypePresent &&
+      isMonthPresent && 
+      isBlockPresent
+    ) {
+      filterTowns.push(town);
+    }
+
+    if (
+      isStreetPresent &&
+      isTownPresent &&
+      isMonthPresent && 
+      isBlockPresent
+    ) {
+      filterFlatTypes.push(flatType);
+    }
+
+    if (
+      isStreetPresent &&
+      isTownPresent &&
+      isFlatTypePresent && 
+      isBlockPresent
+    ) {
+      filterMonths.push(yearMonth);
+    }
+
+
+    if (
+      isStreetPresent &&
+      isTownPresent &&
+      isFlatTypePresent &&
+      isMonthPresent &&
+      isBlockPresent
+    ) {
+      filteredTransaction.push(transaction);
+    }
+
+
+    
+  }
+  
+  // filterBlocks.sort();
+  // filterStreets.sort();
+  // filterTowns.sort();
+  // filterFlatTypes.sort();
+  // filterMonths.sort();
+
 
   return {
-    filterMonths,
-    filterTowns,
-    filterStreets,
-    filterBlocks,
-    filterFlatTypes,
-    filteredTransaction,
+    filterMonths :[...new Set(filterMonths)].sort(),
+    filterTowns : [...new Set(filterTowns)].sort(),
+    filterStreets : [...new Set(filterStreets)].sort(),
+    filterBlocks : [...new Set(filterBlocks)].sort(),
+    filterFlatTypes : [...new Set(filterFlatTypes)].sort(),
+    filteredTransaction
   }
 };
