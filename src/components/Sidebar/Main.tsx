673cc984c266a61fb7e8a3ae0c05f654f0de1134
abdul0398 <abdulrkhan570@gmail.com>
2024-05-06 15:54:24 +0530

    Style:Sidebar design

--- src/components/Sidebar/Main.tsx
+++ src/components/Sidebar/Main.tsx
@@ -24,7 +24,6 @@ export default function Sidebar({scrollHandler, selected, isOpen, setIsOpen}:{sc
         setIsOpen(!isOpen);
     };
 
-    console.log(selected);
     return (
         <div>
             <button onClick={toggleSidebar} type="button" className="absolute top-0 right-0 p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
@@ -34,38 +33,38 @@ export default function Sidebar({scrollHandler, selected, isOpen, setIsOpen}:{sc
                 </svg>
             </button>
             {isOpen && (
-                <aside id="default-sidebar" className="sm:w-1/6 fixed w-1/2 top-0 left-0 z-40 h-screen" aria-label="Sidebar">
+                <aside id="default-sidebar" className="w-1/2 md:w-1/5 lg:w-1/6 fixed rounded-3xl left-3 z-40 h-[90vh] top-1/2 translate-y-[-50%]" aria-label="Sidebar">
                     <Image
                         src='/building.png'
                         width={0}
                         height={0}
-                        className="absolute bottom-0"
+                        className="absolute bottom-0 rounded-3xl"
                         alt="building"
                         sizes="100vw"
                         style={{ width: '100%', height: '50%' }}
                     />
-                    <div className="h-full py-4 overflow-y-auto bg-[#303dd7] dark:bg-gray-800 text-white">
+                    <div className="h-full rounded-3xl py-4 overflow-y-auto bg-[#303dd7] dark:bg-gray-800 text-white">
                         <div className="mb-5">
                             <a href="#" className="flex items-center p-2 pointer-events-none">
                                 <span className="ms-3 text-2xl font-sans">HDB Rental</span>
                             </a>
                         </div>
                         <hr />
-                        <ul className="space-y-2 font-medium mt-11">
+                        <ul className="space-y-2 font-medium mt-11 ">
                             <li>
-                                <a onClick={(event)=>scrollHandler(event)} data-target="filters" className={`flex items-center p-2  ${selected == "filters"? "bg-white text-black":"hover:bg-white hover:text-black"}`}>
+                                <a onClick={(event)=>scrollHandler(event)} data-target="filters" className={`flex items-center ms-auto p-2 w-[90%] rounded-s-3xl  ${selected == "filters"? "bg-white text-black":"hover:bg-white hover:text-black"}`}>
 
                                     <span className="flex-1 ps-5 ms-3 text-xl font-sans whitespace-nowrap cursor-pointer">Filters</span>
                                 </a>
                             </li>
                             <li>
-                                <a onClick={(event)=>scrollHandler(event)} data-target="transactions" className={`flex items-center p-2  ${selected == "transactions"? "bg-white text-black":"hover:bg-white hover:text-black"}`}>
+                                <a onClick={(event)=>scrollHandler(event)} data-target="transactions" className={`flex items-center ms-auto p-2 w-[90%] rounded-s-3xl  ${selected == "transactions"? "bg-white text-black":"hover:bg-white hover:text-black"}`}>
 
                                     <span className="flex-1 ps-5 ms-3 text-xl whitespace-nowrap font-sans cursor-pointer">Transactions</span>
                                 </a>
                             </li>
                             <li>
-                                <a onClick={(event)=>scrollHandler(event)} data-target="graphs" className={`flex items-center p-2  ${selected == "graphs"? "bg-white text-black":"hover:bg-white hover:text-black"}`}>
+                                <a onClick={(event)=>scrollHandler(event)} data-target="graphs" className={`flex items-center ms-auto p-2 w-[90%] rounded-s-3xl ${selected == "graphs"? "bg-white text-black":"hover:bg-white hover:text-black"}`}>
 
                                     <span className="flex-1 ps-5 ms-3 text-xl whitespace-nowrap font-sans cursor-pointer">Graphs</span>
                                 </a>
